07b78037a3cdb0ab68da18dbb0a628294fda1ad9
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-08-10 09:28:50 -0300

    install dependencies - config project

---
+++ hardhat.config.ts
@@ -0,0 +1,22 @@
+import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+};
+
+export default config;
---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Decentra-Blog</title>
+        <meta name="description" content="Web3 blog" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+         Blog
+      </main>
+    </div>
+  )
+}
+
+export default Home
